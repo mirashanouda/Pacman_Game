436baff8b7c413ce1340f9ecccc70d8d3c20359b
mira shanouda <mira.shanouda@aucegypt.edu>
2020-11-17 12:38:40 +0200

    Header file includes all the libraries and fixed global defined values

---
+++ Header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER
+#define HERADER
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+using namespace sf;
+using namespace std;
+#define row  21
+#define column 19
+#define matrix 189
+#define infinity 999
+
+#endif
